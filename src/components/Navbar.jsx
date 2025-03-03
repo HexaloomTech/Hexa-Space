b0e2a914e21cf8e5fce2fbbe919f70952ecedbc2
Jaycode01 <josephlamidi01@gmail.com>
2025-03-03 11:23:23 +0100

    03-----03

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,7 @@ const Navbar = () => {
   return (
     <nav className="text-white p-4 fixed w-full top-0 z-10">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex gap-2">
-      <img src={logo} alt="logo" className="w-15"/>
-        <h1 className="text-xl font-bold flex text-violet-400 items-center">Hexaloom Technologies</h1>
-        </div>
+      <img src={logo} alt="logo" className="w-24"/>
         {/* Hamburger & Close Icon */}
         <button
           onClick={() => setMenuOpen(!menuOpen)}
@@ -20,7 +17,7 @@ const Navbar = () => {
         </button>
 
         {/* Desktop Menu */}
-        <ul className="hidden md:flex space-x-6">
+        <ul className="hidden md:flex text-lg space-x-6">
           {["Home", "About", "projects", "Blog", "Team", "Contact"].map((item) => (
             <li key={item}>
               <a href={`#${item.toLowerCase()}`} className="hover:text-violet-400">
@@ -33,7 +30,7 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       {menuOpen && (
-        <ul className="md:hidden text-center py-4 space-y-4">
+        <ul className="md:hidden text-center  py-4 space-y-4">
           {["Home", "About", "projects", "Blog", "Team", "Contact"].map((item) => (
             <li key={item}>
               <a
